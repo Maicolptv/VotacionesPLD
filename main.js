6b8d9ef3de37de9752fd6422c128707fee1a7ae5
Michael Torres <maicolfeik@gmail.com>
2019-10-22 12:11:54 -0400

    alerts

--- main.js
+++ main.js
@@ -7,9 +7,19 @@ function activar() {
   votos.innerHTML = votosG;
   votosG++;
   votosG * 2;
+  if (votosG > 16) {
+    var alerta = document.getElementById('alerta');
+    alerta.innerHTML =
+      '<div class="alert alert-light" role="alert">El Ganador Es Gonzalo!</div>';
+  }
 }
 function activar1() {
   var votos = document.getElementById('votosL');
   votos.innerHTML = votosL;
   votosL++;
+  if (votosL > 16) {
+    var alerta = document.getElementById('alerta');
+    alerta.innerHTML =
+      '<div class="alert alert-light" role="alert">El Ganador Es Leonel!</div>';
+  }
 }
