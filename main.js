c86fc29d96d3673035f3bb9eb388cc3727bd65c6
Michael Torres <maicolfeik@gmail.com>
2019-10-22 10:43:31 -0400

    .

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+var votosG;
 function activar() {
   let div = document.getElementById('votosG');
-  div.innerHTML = agregarvotosG;
-}
-function agregarvotosG() {
-  for (var i = 0; i < 100; i++) {
-    i + 1;
+  div.innerHTML = aumentarvotosG(i);
+  var votosG = 0;
+
+  function aumentarvotosG(votosG) {
+    for (i = 0; i < 5; i++) {
+      votosG = 0;
+    }
   }
+}
+function activar1() {
+  let div = document.getElementById('votosL');
+  div.innerHTML = votosG;
+  var votosG;
+
+  votosL = 0;
 }
