268644ab94f824dd339d7d835b4f29c99dcc5a0f
Michael Torres <maicolfeik@gmail.com>
2019-10-22 09:57:30 -0400

    .

---
+++ main.js
@@ -0,0 +1,9 @@
+function activar() {
+  let div = document.getElementById('votosG');
+  div.innerHTML = agregarvotosG;
+}
+function agregarvotosG() {
+  for (var i = 0; i < 100; i++) {
+    i + 1;
+  }
+}
