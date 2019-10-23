f11e9e86bc42527770c105862c665cb182caade6
Maicolptv <53354229+Maicolptv@users.noreply.github.com>
2019-10-23 11:54:16 -0400

    .

--- main.js
+++ main.js
@@ -8,33 +8,11 @@ function activar() {
   var gonzalo = document.getElementById('gonzalo');
   votos.innerHTML = votosG;
   votosG++;
-
-  if (votosG > 16) {
-    var alerta = document.getElementById('alerta');
-    alerta.innerHTML =
-      '<div class="alert alert-light" role="alert">El Ganador Es Gonzalo!</div>';
-  }
-  gonzalo.addEventListener('click', () => {
-    speechSynthesis.onvoiceschanged = () => {
-      const text = 'Has votado por Gonzalo Castillo, gracias por su colaboracion'
-      const synth = speechSynthesis
-      const voices = synth.getVoices()
-      const utterThis = new SpeechSynthesisUtterance(text)
-      utterThis.voice = voices.find(v => v.name === 'Ellen')
-      utterThis.rate = 0.7
-      synth.speak(utterThis)
-      return speechSynthesis
-    }
-  })
 }
 
 function activar1() {
   var votos = document.getElementById('votosL');
+  var leonel = document.getElementById('leonel');
   votos.innerHTML = votosL;
   votosL++;
-  if (votosL > 16) {
-    var alerta = document.getElementById('alerta');
-    alerta.innerHTML =
-      '<div class="alert alert-light" role="alert">El Ganador Es Leonel!</div>';
-  }
 }
